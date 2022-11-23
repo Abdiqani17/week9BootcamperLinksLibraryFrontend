fade06a393f957b4d67c12b98e4385971355324b
Abdiqani Mohamed <Mohamedabdiqani321@gmail.com>
2022-11-23 12:15:32 +0000

    doing carousel

--- bootcamp-links-library/src/Components/Carousel/Carousel.js
+++ bootcamp-links-library/src/Components/Carousel/Carousel.js
@@ -6,19 +6,19 @@ import Card from "../Card/Card"
 function Carousel(props) {
   console.log(props.title)
   return (
-      <div className="carousel-container">
-          <div className="carousel-wrapper">
-              <div className="carousel-content-wrapper">
-                  <div className="carousel-content">
+    //   <div className="carousel-container">
+    //       <div className="carousel-wrapper">
+    //           <div className="carousel-content-wrapper">
+    //               <div className="carousel-content">
                       <Card key={props.link_id}
-                            subjectIcon={props.icon}
+                            subjectIcon={props.subjectIcon}
                             title={props.title}
                             description={props.description}
                             link={props.link}/>
-                  </div>
-              </div>
-          </div>
-      </div>
+    //               </div>
+    //           </div>
+    //       </div>
+    //   </div>
   )
 }
 
